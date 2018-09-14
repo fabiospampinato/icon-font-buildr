d0fb979a49042fe5fa2febe9f57d117c35deff3e
Fabio Spampinato <spampinabio@gmail.com>
2018-09-14 02:41:12 +0200

    Improved CLI

--- src/bin.ts
+++ src/bin.ts
@@ -2,16 +2,35 @@
 
 /* IMPORT */
 
-import * as _ from 'lodash';
+import * as caporal from 'caporal';
 import * as minimist from 'minimist';
+import * as readPkg from 'read-pkg-up';
 import * as rdf from 'require-dot-file';
+import * as updateNotifier from 'update-notifier';
 import IconFontBuildr from '.';
 
-/* CONFIG */
+/* CLI */
 
-const argv = minimist ( process.argv.slice ( 2 ) ),
-      config = ( argv.config && require ( argv.config ) ) || rdf ( 'icon_font.json', process.cwd () ) || {};
+async function CLI () {
 
-/* ICON FONT BUILDR */
+  const {pkg} = await readPkg ({ cwd: __dirname });
 
-new IconFontBuildr ( config ).build ();
+  updateNotifier ({ pkg }).notify ();
+
+  caporal
+    .version ( pkg.version )
+    .option ( '--config <path>', 'Path to the config file' )
+    .action ( () => {
+
+      const argv = minimist ( process.argv.slice ( 2 ) ),
+            config = ( argv.config && require ( argv.config ) ) || rdf ( 'icon_font.json', process.cwd () ) || {};
+
+      new IconFontBuildr ( config ).build ();
+
+    });
+
+  caporal.parse ( process.argv );
+
+}
+
+CLI ();
